318031f8667acc755f0138ff931b76637072f5be
Danyalfa <dalfaro@sintelti.mx>
2023-10-10 15:31:34 -0600

    fixing

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:'https://daniel13alfa.github.io/Portfolio/'
+  base:'https://Daniel13Alfa.github.io/Portfolio/'
 })
