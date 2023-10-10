96a6de01f7eabcb1879899d75d07ccbb3cae0a5a
Danyalfa <dalfaro@sintelti.mx>
2023-10-10 15:54:04 -0600

    test

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -17,7 +17,7 @@ const Content = () => {
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi (example).
+            a id nisi.
           </p>
         </div>
       </div>
