74123193ef248902133c4c474c06d4b4bd278644
danyAlfa1 <dalfaro@leitmotiv.mx>
2024-01-19 13:42:40 -0600

    se agregan cambios de estructura

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
-
+import Skills from "./components/Skills";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -14,6 +14,26 @@ function App() {
       <Header />
       <div>
         <Content />
+      </div>
+      <p className="text-center text-5xl">OBJETIVE</p>
+      <br></br>
+      <div className="w-full flex justify-center items-center">
+        <div className="  w-9/12 pt-2 pb-2">
+          <h1 className="text-center">
+            Using the tools and knowledge required for my profession, with the
+            purpose of helping companies to reach their goals and meet the
+            requirements necessary. By contributing my skills as well as
+            learning for others. In order, to find a successful, and mutual
+            agreement. In result, give me the opportunity to grow as a person
+            and as an engineer. (Looking for employment opportunities in
+            Software Development/Engineering).
+          </h1>
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+      <div>
+        <Skills />
       </div>
       <Footer />
     </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,215 @@
 import React from "react";
 import Header from "./Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import {
+  faJava,
+  faJs,
+  faPython,
+  faReact,
+  faCss3Alt,
+  faHtml5,
+  faAngular,
+  faNode,
+  faGithub,
+  faGitlab,
+  faJira,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Skills = () => {
   return (
-    <div className="dark:bg-gray-900 bg-center">
-      <Header />
+    <div className="dark:bg-gray-900">
+      <p className="text-center text-5xl">SKILLS</p>
+      <div className="CardInfo w-12/12 dark:bg-gray-900 mt-8 rounded-xl">
+        <div className="flex justify-between items-center pl-10 pr-10">
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faJava} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                JAVA
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faJs} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                JAVA SCRIPT
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faPython} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                PYTHON
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">C++</p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                C++
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faReact} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                REACT
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faCss3Alt} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                CSS
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="CardInfo w-12/12 dark:bg-gray-900 mt-8 rounded-xl">
+        <div className="flex justify-between items-center pl-10 pr-10">
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faHtml5} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                HTML
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faAngular} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                ANGULAR
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faNode} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                NODE JS
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faDatabase} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                SQL
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faDatabase} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                MONGO DB
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faDatabase} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                MY SQL
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="CardInfo w-12/12 dark:bg-gray-900 mt-8 rounded-xl">
+        <div className="flex justify-between items-center pl-10 pr-10">
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faGithub} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                GITHUB
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faGitlab} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                GITLAB
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faJira} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                JIRA
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faDatabase} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                SQL
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faDatabase} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                MONGO DB
+              </h5>
+            </div>
+          </div>
+          <div className="dark:bg-gray-900 pt-2 pb-2 w-2/12 p-3">
+            <div className="p-6 bg-white border border-gray-900 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p className="text-center text-5xl">
+                <FontAwesomeIcon icon={faDatabase} />
+              </p>
+              <h5 className="text-center mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                MY SQL
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
