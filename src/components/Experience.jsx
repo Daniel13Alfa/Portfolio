060707efa85a1f8202e09c9dfdbd2e2979a47312
danyAlfa1 <dalfaro@leitmotiv.mx>
2025-01-29 20:51:43 -0500

    experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Experience = () => {
   return (
@@ -7,8 +8,8 @@ const Experience = () => {
       <Header />
       <p className="text-center text-5xl">Experience & Education</p>
       <br></br>
-      <div className="w-full flex">
-        <div className=" content-center w-11/12 pt-2 ">
+      <div className="w-full">
+        <div className=" content-center w-12/12 pt-2 ">
           <h1 className="text-center">
             Using the tools and knowledge required for my profession, with the
             purpose of helping companies to reach their goals and meet the
@@ -19,7 +20,53 @@ const Experience = () => {
             Software Development/Engineering).
           </h1>
         </div>
+        <br></br>
+        <div className="justify-items-end">
+          <div className="w-6/12 rounded-xl  hover:bg-gray-950 first-line:hover:text-red-500">
+            <div className="justify-items-end">
+              <div className="text-center text-3xl w-8/12 ">
+                <h1>SANTANDER</h1>
+              </div>
+            </div>
+            <br></br>
+            <div className="flex">
+              <div className="w-4/12 rounded-xl text-center">
+                <h1 className="text-xl">2024 - present</h1>
+              </div>
+              <div className="w-8/12 rounded-xl">
+                <h2 className="text-center">
+                  Using the tools and knowledge required for my profession, with
+                  the purpose of helping companies to reach their goals and meet
+                  the requirements necessary. By contributing my skills as well
+                  as learning for others. In order, to find a successful, and
+                  mutual agreement. In result, give me the opportunity to grow
+                  as a person and as an engineer. (Looking for employment
+                  opportunities in Software Development/Engineering).
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <div className="justify-items-center">
+          <div className="flex card lg:card-side bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+                alt="Album"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import Header from "./Header";
+import imageIlutrative from "../assets/Images/diagrama_rest.png";
+import Footer from "./Footer";
 
 const About = () => {
   return (
-    <div className="dark:bg-black bg-center text-white">
+    <div className="dark:bg-black text-white w-12/12 ">
       <Header />
+      <br></br>
+      <div className="justify-items-center">
+        <div className="flex card lg:card-side bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+              alt="Album"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New album is released!</h2>
+            <p>Click the button to listen on Spotiwhy app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Listen</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <h1>Hi this is Projects</h1>
       <a href="https://daniel13alfa.github.io/HospitalWeb/"> my web</a>
+      <Footer />
     </div>
   );
 };
